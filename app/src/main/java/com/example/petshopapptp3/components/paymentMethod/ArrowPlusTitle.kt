3c1bc32daedfed490c3a88c8bcdaa4d927cac32e
ManuelBranca <manubranca0227@gmail.com>
2025-05-31 23:38:41 -0300

    Avances PaymentMethod

---
+++ app/src/main/java/com/example/petshopapptp3/components/paymentMethod/ArrowPlusTitle.kt
@@ -0,0 +1,31 @@
+package com.example.petshopapptp3.components.paymentMethod
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ArrowTitle (onBack: () -> Unit = {}){
+    Row(
+        Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = onBack) {
+            Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        PaymentTitle()
+    }
+}
---
+++ app/src/main/java/com/example/petshopapptp3/screens/paymentMethod/success/PaymentSuccess.kt
@@ -0,0 +1,43 @@
+package com.example.petshopapptp3.screens.paymentMethod.success
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.petshopapptp3.components.buttons.StartButton
+import com.example.petshopapptp3.components.shared.SubtitleSection
+import com.example.petshopapptp3.components.shared.TitleSection
+
+@Composable
+fun PaymentSucces (){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp, vertical = 16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column (
+            modifier = Modifier.padding(top = 32.dp)
+        ) {
+            TitleSection("Payment\nSuccess!")
+            Spacer(modifier = Modifier.height(10.dp))
+            SubtitleSection("your order is being prepared by the shop, the courier will send it to your address")
+        }
+
+        StartButton("Go Home")
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowPaymentSuccess(){
+    PaymentSucces()
+}
